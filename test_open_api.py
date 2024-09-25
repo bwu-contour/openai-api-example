c1eca74d4879f74010096529d08c361753af586e
bwu-contour <bing.wu@chase.com>
2024-09-25 13:45:13 -0700

    handle HTTPS

--- test_open_api.py
+++ test_open_api.py
@@ -5,12 +5,18 @@ ENDPOINT = "http://localhost:8080"
 
 @pytest.mark.repeat(10)  # Run the test 10 times
 def test_can_call_endpoint():
-    response = requests.get(ENDPOINT)
+    if "https" in ENDPOINT.lower():
+        response = requests.get(ENDPOINT, verify=False)
+    else:
+        response = requests.get(ENDPOINT)
     assert response.status_code == 200
 
 @pytest.mark.repeat(10)  # Run the test 10 times
 def test_generate_snippet_keywords():
-    response = requests.get(ENDPOINT + "/generate_snippet_and_keywords?prompt=JPMorgan%20Chase")
+    if "https" in ENDPOINT.lower():
+        response = requests.get(ENDPOINT + "/generate_snippet_and_keywords?prompt=JPMorgan%20Chase", verify=False)
+    else:
+        response = requests.get(ENDPOINT + "/generate_snippet_and_keywords?prompt=JPMorgan%20Chase")
     assert response.status_code == 200
     
     #data = response.json();
